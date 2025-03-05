c31f4aef45ecd1d592468f6f0bb604cc7b23b55a
Ijaz Ali <ijazalideveloper@gmail.com>
2025-03-05 23:52:38 +0400

    Due to turbopack tailwind having problem - Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-      extend: {
-        colors: {
-          black: '#322625',
-          grey: '#ebebeb',
-          blue: '#c0e3e5',
-          yellow: '#fdc936',
-        },
-        fontFamily: {
-          neutra: ['Neutra Text', 'sans-serif'],
-        },
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#322625",
+        grey: "#ebebeb",
+        blue: "#c0e3e5",
+        yellow: "#fdc936",
+      },
+      fontFamily: {
+        neutra: ["Neutra Text", "sans-serif"],
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
